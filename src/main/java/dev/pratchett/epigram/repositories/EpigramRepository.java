df9a3f73d9b3b480c5d9f7684f6e95af93294329
Alexis Pratchett <alexispratchett@gmail.com>
2025-05-26 10:30:56 -0500

    Adding volume so Postgres will actually persist

--- src/main/java/dev/pratchett/epigram/repositories/EpigramRepository.java
+++ src/main/java/dev/pratchett/epigram/repositories/EpigramRepository.java
@@ -4,9 +4,12 @@ package dev.pratchett.epigram.repositories;
 import jakarta.transaction.Transactional;
 import dev.pratchett.epigram.models.Epigram;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Transactional
 @Repository
 public interface EpigramRepository extends JpaRepository<Epigram, Integer> {
+//    @Query(value = "SELECT e FROM epigrams e ORDER BY random()")
+//    public Epigram getRandomEpigram();
 }
